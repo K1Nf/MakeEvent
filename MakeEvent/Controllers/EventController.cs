c759614d9b7ffa9230c506aad584c3dc7dd45730
K1Nf <nikitka.repin.2017@mail.ru>
2023-12-13 12:06:20 +0500

    FirstCommit

---
+++ MakeEvent/Controllers/EventController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MakeEvent.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult EventPage()
+        {
+            return View();
+        }
+    }
+}
