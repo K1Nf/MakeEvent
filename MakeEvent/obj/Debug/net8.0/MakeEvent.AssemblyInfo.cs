5e571361e2f842e209c51f6606bffc237bb28197
K1Nf <nikitka.repin.2017@mail.ru>
2023-12-15 11:57:47 +0500

    Доработка страниц

--- MakeEvent/obj/Debug/net8.0/MakeEvent.AssemblyInfo.cs
+++ MakeEvent/obj/Debug/net8.0/MakeEvent.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MakeEvent")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0019a302e3b0f1fd3f560b875c6cac4b13819ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MakeEvent")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MakeEvent")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
